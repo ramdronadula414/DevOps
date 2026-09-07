ae1ccf58ce922c4a829a59396f20e7c7697a4787
ramdronadula414 <ramanjaneyuludronadula4676@gmail.com>
2026-09-07 05:56:37 -0400

    java files are added

---
+++ HelloJenkins.java
@@ -0,0 +1,9 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("================================");
+        System.out.println("Hello from Jenkins!");
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+        System.out.println("Jenkins executed my Java program.");
+        System.out.println("================================");
+    }
+}
